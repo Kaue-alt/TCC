6a74e26ed8f2b67104825e79fdb941a4124cb3ce
Kaue Joaquim <kaue.uriel@hotmail.com>
2021-03-24 17:59:17 -0300

    Combo

---
+++ Projeto 5/Assets/Scripts/Combos.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combos : MonoBehaviour
+{
+    Animator animator;
+
+    int clicks;
+    bool canClick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        clicks = 0;
+        canClick = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Combo();
+        }
+    }
+
+    void Combo()
+    {
+        if (canClick)
+        {
+            clicks++;
+        }
+        if (clicks == 1)
+        {
+            animator.SetInteger("Attack", 1);
+        }
+    }
+
+    void ComboManager()
+    {
+        canClick = false;
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1")&& clicks == 1)
+        {
+            animator.SetInteger("Attack", 0);
+            canClick = true;
+            clicks = 0;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1") && clicks >= 2)
+        {
+            animator.SetInteger("Attack", 2);
+            canClick = true;
+           
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_2") && clicks == 2)
+        {
+            animator.SetInteger("Attack", 0);
+            canClick = true;
+            clicks = 0;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_2") && clicks >= 3)
+        {
+            animator.SetInteger("Attack", 3);
+            canClick = true;
+
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_3"))
+        {
+            animator.SetInteger("Attack", 0);
+            canClick = true;
+            clicks = 0;
+        }
+    }
+}
