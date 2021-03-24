778a6416b193206c94b9b5c07064ba86b761d237
Kaue Joaquim <kaue.uriel@hotmail.com>
2021-03-24 03:10:47 -0300

    Animação do Personagem

--- Projeto 5/Assets/Scripts/Movimentacao.cs
+++ Projeto 5/Assets/Scripts/Movimentacao.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class Movimentacao : MonoBehaviour
 {
     public float speed;
-    public Vector3 dir;
-    public Rigidbody rb;
-    public Transform collisionPivot;
-    public float radius;
-    public LayerMask layer;
     public float jumpForce;
+    public float radius;
+    private float horizontal;
+    //private float vertical;
+
+    public LayerMask layer;
+    public Vector3 dir;
+
+    public Rigidbody rb;
+    //public Transform collisionPivot;    
     public MeshRenderer mr;
-    public CapsuleCollider col;
+    public CapsuleCollider col;  
+    private Animator animator;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
+        this.animator = GetComponent<Animator>();
     }
     
     // Update is called once per frame
     void Update()
     {
-
+        this.Animations();
         // =============================== CONTROLE PELO TECLADO E MOUSE =============================================================
-                   
+
         // Pulo tecla Space
         if (Input.GetButtonDown("Jump"))
         {
@@ -42,20 +49,30 @@ public class Movimentacao : MonoBehaviour
         dir.y = rb.velocity.y;   // Eixo y (setando para considerar a física do Rigidbody)
         dir.z = 0;
 
+        //vertical = 0;
+        horizontal = dir.x;
+
         // Espelha o personagem no sentido que está indo quando se movimentar
         //if (Input.GetAxisRaw("Horizontal") > 0)
         //{
-           // mr.flipX = true;
+        // mr.flipX = true;
         //}
 
         //if (Input.GetAxisRaw("Horizontal") < 0)
         //{
-           // mr.flipX = false;
+        // mr.flipX = false;
         //}
 
         // =============================== CONTROLE PELO JOYSTICK ====================================================================
 
     }
+    private void Animations()
+    {
+        this.animator.SetFloat("Horizontal", horizontal);
+        //this.animator.SetFloat("Vertical", vertical);
+    }
+
+
 
     void FixedUpdate() //Essa função roda a cada 0.02 segundos, melhorando o controle na movimentação considerando elementos da física
     {
