f3ac28bce76476b4dafd41abec42298c04a6798f
Kaue Joaquim <kaue.uriel@hotmail.com>
2021-03-30 20:16:11 -0300

    Objetos Quebrados

---
+++ Projeto 5/Assets/Scripts/BreakingObjects.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingObjects : MonoBehaviour
+{
+    public GameObject brokenObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 oldPos = transform.position;
+            Instantiate(brokenObject, oldPos, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollision(Collision collider)
+    {
+        if (collider.gameObject.tag == "Weapon")
+        {
+            Vector3 oldPos = transform.position;
+            Instantiate(brokenObject, oldPos, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
