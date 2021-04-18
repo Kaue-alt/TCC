70942167acdbe8d3d8f283f51df55c2629fae5a1
pedroborges13 <pedroborges13@users.noreply.github.com>
2021-04-18 06:34:50 -0300

    BUILD DO PLAYTEST
    PLAYTEST

--- Projeto 5/Assets/Scripts/vidaEnemy.cs
+++ Projeto 5/Assets/Scripts/vidaEnemy.cs
@@ -6,12 +6,19 @@ public class vidaEnemy : MonoBehaviour
 {
     public float life = 100f;
 
+    public AudioSource audioSourceMonster;
+
     void Update()
     {
         if (life <= 0f)
         {
             Debug.Log("Inimigo Morto");
             Destroy(gameObject);
+            playAudio();
+        }
+        void playAudio()
+        {
+            audioSourceMonster.Play();
         }
     }
 }
