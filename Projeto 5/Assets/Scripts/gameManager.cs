335efe01a1ab692d3f34912be89a2407b2c91264
Kaue Joaquim <kaue.uriel@hotmail.com>
2021-06-02 20:49:59 -0300

    Texturas do esgoto

---
+++ Projeto 5/Assets/Scripts/gameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    /*public string objectID;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        object= name + transform.position.ToString();
+    }
+    void Start()
+    {
+        for (int i-0; i < Object.FindObjectsOfType<gameManager>().Length; i++)
+        {
+            if(Object.FindObjectsOfType<gameManager>()[i] != this)
+            {
+                if (Object.FindObjectsOfType<gameManager>()[i].objectID == objectID)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
