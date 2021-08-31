625b84194d5c7c68fa71d8cf419b1355bc82d9d4
Kaue-alt <55499541+Kaue-alt@users.noreply.github.com>
2021-08-31 17:29:04 -0300

    Armadilhas

---
+++ Projeto 5/Assets/triggerTraps.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerTraps : MonoBehaviour
+{
+   public GameObject trigger;
+   public  GameObject trap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Colidiu");
+            trap.transform.position = new Vector3(0, 10 * Time.deltaTime, 0); 
+        }
+        
+
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            trap.transform.position = new Vector3(trap.transform.position.x, trap.transform.position.y + 50 * Time.deltaTime, trap.transform.position.z);
+        }
+    }
+}
---
+++ Projeto 5/Assets/triggerTrapsII.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerTrapsII : MonoBehaviour
+{
+   
+    public GameObject trigger;
+    public GameObject trap;
+  /*public GameObject trapA;
+    public GameObject trapB;
+    public GameObject trapC;
+    public GameObject trapD;
+    public GameObject trapE;
+    public GameObject trapF;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+
+            //trapA.transform.position = new Vector3(trapA.transform.position.x, trapA.transform.position.y - 100 * Time.deltaTime, trapA.transform.position.z);
+            trap.transform.position = new Vector3(trap.transform.position.x, trap.transform.position.y -100 * Time.deltaTime, trap.transform.position.z);
+
+
+        }
+
+       
+    }
+}
