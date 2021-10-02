55660a584b1b0632c2985cdf00b32a05d214b648
pedroborges13 <pepehb2001@gmail.com>
2021-10-02 16:39:31 -0300

    Comandante diálogo e textos p/ interação
    -Diálogo com a comandante feito.
    -Mensagem de interação com o "E" em todas as transições.

---
+++ Projeto 5/Assets/Scripts/Tutorial/inteEsgoto.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inteEsgoto : MonoBehaviour
+{
+    public Image tutorialBackgrounddddd;
+
+    public Text textoInteragirEsgoto;
+
+    public GameObject _tutorialBackgrounddddd;
+    public GameObject _textInteragirEsgoto;
+
+    //public AudioSource runSound;
+
+    void Start()
+    {
+        tutorialBackgrounddddd.enabled = false;
+        textoInteragirEsgoto.enabled = false;
+
+        _textInteragirEsgoto.SetActive(true);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Está dentro do Pensamento");
+            textoInteragirEsgoto.enabled = true;
+            tutorialBackgrounddddd.enabled = true;
+            //runSound.Stop();
+        }
+    }
+
+    //void OnTriggerEnter(Collider other)
+}
