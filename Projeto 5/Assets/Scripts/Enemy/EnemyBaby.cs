781fa04c33235c172baf051aea89c1c1267cd28d
BeatrizUrgal <beaurgalmoreno@gmail.com>
2021-11-16 02:32:43 -0300

    Animação Baby

--- Projeto 5/Assets/Scripts/Enemy/EnemyBaby.cs
+++ Projeto 5/Assets/Scripts/Enemy/EnemyBaby.cs
@@ -10,17 +10,19 @@ public class EnemyBaby : MonoBehaviour
     public NavMeshAgent agent;
     public float distance;
 
-    public Transform ProjetilPrefab;
-    Transform shotSpawn;
-    Transform enemyBaby;
+    public GameObject ProjetilPrefab;
+    public Transform shotSpawn;
+    //public Transform enemyBaby;
 
-    public GameObject enemy;
+    public GameObject Babyenemy;
+
+    private Animator animaBaby;
 
 
     void Awake()
     {
-        shotSpawn = transform.Find("shotSpawn");
-        shotSpawn = transform.Find("enemyBaby");
+        //shotSpawn = transform.Find("shotSpawn");
+        //shotSpawn = transform.Find("enemyBaby");
 
         
     }
@@ -29,8 +31,11 @@ public class EnemyBaby : MonoBehaviour
     {
         // Inimigo encontra o player pela tag
         player = GameObject.FindWithTag("Player");
+        animaBaby = Babyenemy.GetComponent<Animator>();
 
-        //enemy.transform.Rotate(-90.0f, 0.0f, -90.0f);
+        animaBaby.SetBool("bIdle", true);
+
+        
     }
 
 
@@ -43,32 +48,37 @@ public class EnemyBaby : MonoBehaviour
         {
             agent.destination = player.transform.position;
             agent.isStopped = false;
+            animaBaby.SetBool("bIdle", false);
+            animaBaby.SetBool("bRun", true);
+            
 
-            //GetComponent<Animator>().SetBool("bRun", true);
-            //GetComponent<Animator>().SetBool("bIdle", false);
-
-            //ShootBullet();
+            ShootBullet();
         }
+        //else
+        //{
+            //animaBaby.SetBool("bRun", false);
+            //animaBaby.SetBool("bIdle", true);
+        //}
 
-        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attacking")) // DEIXA O INIMIGO PARADO ENQUANTO ATACA
-        {
-            agent.isStopped = true;
-        }
+        //if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attacking")) // DEIXA O INIMIGO PARADO ENQUANTO ATACA
+        //{
+            //agent.isStopped = true;
+        //}
 
-        if (Vector3.Distance(transform.position, player.transform.position) > distance)
-        {
+        //if (Vector3.Distance(transform.position, player.transform.position) > distance)
+        //{
             //GetComponent<Animator>().SetBool("bRun", false);
            //GetComponent<Animator>().SetBool("bIdle", true);
-        }
+       // }
 
         // =============================================================================================================
     }
 
-    /*void ShootBullet()
+    void ShootBullet()
     {
-        Transform projetilobj = Instantiate(ProjetilPrefab, shotSpawn.position, shotSpawn.rotation);
-        Destroy(projetilobj.gameObject, 8f);
+        Instantiate(ProjetilPrefab, shotSpawn.position, shotSpawn.rotation);
+        Destroy(ProjetilPrefab.gameObject, 8f);
 
-        projetilobj.GetComponent<Projetil>().SetDirection(enemyBaby.GetForwardDirection());
-    }*/
+        //projetilobj.GetComponent<Projetil>().SetDirection(enemyBaby.GetForwardDirection());
+    }
 }
