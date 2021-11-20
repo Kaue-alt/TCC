010a93e826a00149afb199f24088ade7df421ea5
marinearti <80015994+marinearti@users.noreply.github.com>
2021-11-20 07:47:19 -0300

    Organização de Fases e Cena Final
    - Criação da cena com as artes finais;
    - Criação de script para sequência de artes e retornar ao menu;
    - Organização da Fase 3;
    - Inclusão de mudança na câmera para as partes do laboratório;
    - Organização da sala do boss;

--- Projeto 5/Assets/Scripts/cameraLab.cs
+++ Projeto 5/Assets/Scripts/cameraLab.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class cameraLab : MonoBehaviour
 {
-    // Start is called before the first frame update
+    cameraFollowPlayer camScript;
+    public GameObject nextCol;
+    public float add;
+
     void Start()
     {
-        
+        camScript = FindObjectOfType<cameraFollowPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StartCoroutine(changeDirection());
+            nextCol.SetActive(true);
+        }
+    }
+
+    IEnumerator changeDirection()
+    {
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        camScript.horizontal += add;
+        yield return new WaitForSecondsRealtime(0.04f);
+        Destroy(this);
     }
 }
---
+++ Projeto 5/Assets/Scripts/finalScenes.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalScenes : MonoBehaviour
+{
+    public GameObject firstScene, secScene, thirdScene, fourthScene, fifthScene;
+    private int cont = 0;
+    private bool cooldown = false;
+
+    public Fade fadeScript;
+
+    void Start()
+    {
+        firstScene.SetActive(true);
+        secScene.SetActive(true);
+        thirdScene.SetActive(true);
+        fourthScene.SetActive(true);
+        fifthScene.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(cont);
+            if (cont == 0)
+            {
+                StartCoroutine(fadeSprite(firstScene));
+                StartCoroutine(setCooldown());
+                cont++;
+            }
+            if (cont == 1 && !cooldown)
+            {
+                StartCoroutine(fadeSprite(secScene));
+                StartCoroutine(setCooldown());
+                cont++;
+            }
+            if (cont == 2 && !cooldown)
+            {
+                StartCoroutine(fadeSprite(thirdScene));
+                StartCoroutine(setCooldown());
+                cont++;
+            }
+            if (cont == 3 && !cooldown)
+            {
+                StartCoroutine(fadeSprite(fourthScene));
+                StartCoroutine(endScene());
+                cont++;
+            }
+        }
+    }
+
+    IEnumerator fadeSprite(GameObject scene)
+    {
+
+        Color alphaScene = scene.GetComponent<SpriteRenderer>().color;
+        alphaScene.a -= 0.2f;
+        scene.GetComponent<SpriteRenderer>().color = alphaScene;
+        yield return new WaitForSecondsRealtime(0.1f);
+        if (alphaScene.a > 0)
+        {
+            StartCoroutine(fadeSprite(scene));
+        }
+    }
+
+    IEnumerator setCooldown()
+    {
+        cooldown = true;
+        yield return new WaitForSecondsRealtime(1.2f);
+        cooldown = false;
+    }
+
+    IEnumerator endScene()
+    {
+        fifthScene.SetActive(true);
+        yield return new WaitForSecondsRealtime(1.6f);
+        fadeScript.Transition("Menu");
+    }
+}
